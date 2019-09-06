9df84d16e6e661997a712885278e2bfdd881a0ca
Alfonso Socarras <asocarras10@gmail.com>
2019-09-06 15:20:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,7 +42,7 @@ end
 
 def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
-  applied_coupons = apply_coupons(hash_cart, )
+  applied_coupons = apply_coupons(hash_cart, coupons)
 end
 
 
