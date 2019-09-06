c16dab50a03b4f1a290a5339534d17bfa8f93bf5
Alfonso Socarras <asocarras10@gmail.com>
2019-09-06 15:12:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,7 @@ end
 
 def apply_clearance(cart)
   cart.each do |product_name, stats|
-    stats[:price] -= stats[:price] * 0.2 if stats[:clearance] == true 
+    stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
     binding.pry 
   end
   cart
